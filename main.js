b62d9bb4fa3189688c2fb51517908a9f28e92f2b
Malle <malaru@matchmysound.com>
2022-01-25 13:35:55 +0200

    practicing attributes

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const product = 'Socks'
+const app = Vue.createApp({
+	data() {
+		return {
+			product: 'Sandals',
+			description: 'nice socks',
+			image: './assets/images/socks_green.jpg',
+			inventory: 1,
+			onSale: false,
+			details: ['50% cotton', '30% wool', '20% polyester'],
+			variants: [
+				{ id: 2234, color: 'green'},
+				{ id: 2235, color: 'blue'}
+			]
+		}
+	},
+});
